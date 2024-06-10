abba0da807eeb5f077d0beef857493b647bda03d
izamulfikri <mfikri765@gmail.com>
2024-06-10 15:01:22 +0700

    Fix Auth Layout and Interaction EditText

--- app/src/main/java/com/bangkit/factha/view/activity/auth/LoginActivity.kt
+++ app/src/main/java/com/bangkit/factha/view/activity/auth/LoginActivity.kt
@@ -2,11 +2,17 @@ package com.bangkit.factha.view.activity.auth
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.opengl.Visibility
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
+import android.text.TextWatcher
 import android.text.method.HideReturnsTransformationMethod
 import android.text.method.PasswordTransformationMethod
+import android.util.Patterns
 import android.view.MotionEvent
 import android.view.View
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,13 +21,18 @@ import com.bangkit.factha.R
 import com.bangkit.factha.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
-
     private var isPasswordVisible = false
     private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
+
+        binding.emailEtLogin.addTextChangedListener(emailTextWatcher)
+        binding.passwordEtLogin.addTextChangedListener(passwordTextWatcher)
+        binding.btnLogin.isEnabled = false
+
+
         setContentView(binding.root)
         setupPasswordToggle()
         setupRegisterNavigation()
@@ -51,10 +62,74 @@ class LoginActivity : AppCompatActivity() {
         })
     }
 
+    private val emailTextWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+        override fun afterTextChanged(s: Editable?) {
+            validateEmail()
+            validateFields()
+        }
+    }
+
+    private val passwordTextWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+        override fun afterTextChanged(s: Editable?) {
+            validatePassword()
+            validateFields()
+        }
+    }
+
+    private fun validateEmail() {
+        val email = binding.emailEtLogin.text.toString().trim()
+        if (TextUtils.isEmpty(email)) {
+            binding.emailAlert.visibility = View.VISIBLE
+            binding.emailAlert.text = getString(R.string.email_can_t_empty)
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            binding.emailAlert.visibility = View.VISIBLE
+            binding.emailAlert.text = getString(R.string.invalid_email)
+        } else {
+            binding.emailAlert.visibility = View.GONE
+        }
+    }
+
+    private fun validatePassword() {
+        val password = binding.passwordEtLogin.text.toString().trim()
+        if (TextUtils.isEmpty(password)) {
+            binding.passwordAlert.text = getString(R.string.password_can_t_empty)
+            binding.passwordAlert.visibility = View.VISIBLE
+        } else if (password.length < MIN_PASSWORD_LENGTH) {
+            binding.passwordAlert.text = getString(R.string.password_must_be_at_least_8_characters_long)
+            binding.passwordAlert.visibility = View.VISIBLE
+        } else {
+            binding.passwordAlert.visibility = View.GONE
+        }
+    }
+
     private fun setupRegisterNavigation() {
         binding.registerTv.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun validateFields(){
+        val email = binding.emailEtLogin.text.toString().trim()
+        val password = binding.passwordEtLogin.text.toString().trim()
+
+        val isEmailValid = !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches()
+        val isPasswordValid = !TextUtils.isEmpty(password) && password.length >= MIN_PASSWORD_LENGTH
+
+        binding.btnLogin.isEnabled = isEmailValid && isPasswordValid
+    }
+
+    companion object {
+        private const val MIN_PASSWORD_LENGTH = 8
     }
 }
--- app/src/main/java/com/bangkit/factha/view/activity/auth/RegisterActivity.kt
+++ app/src/main/java/com/bangkit/factha/view/activity/auth/RegisterActivity.kt
@@ -3,8 +3,12 @@ package com.bangkit.factha.view.activity.auth
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
+import android.text.TextWatcher
 import android.text.method.HideReturnsTransformationMethod
 import android.text.method.PasswordTransformationMethod
+import android.util.Patterns
 import android.view.MotionEvent
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -18,8 +22,13 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
+        binding.emailEtRegister.addTextChangedListener(emailTextWatcher)
+        binding.passwordEtRegister.addTextChangedListener(passwordTextWatcher)
+        binding.nameEtRegister.addTextChangedListener(nameTextWatcher)
+        binding.btnRegister.isEnabled = false
+
+        setContentView(binding.root)
         setupPasswordToggle()
         setupLoginNavigation()
     }
@@ -53,5 +62,94 @@ class RegisterActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private val nameTextWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+        override fun afterTextChanged(s: Editable?) {
+            validateName()
+            validateFields()
+        }
+    }
+
+    private val emailTextWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+        override fun afterTextChanged(s: Editable?) {
+            validateEmail()
+            validateFields()
+        }
+    }
+
+    private val passwordTextWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+        override fun afterTextChanged(s: Editable?) {
+            validatePassword()
+            validateFields()
+        }
+    }
+
+    private fun validateName() {
+        val email = binding.nameEtRegister.text.toString().trim()
+        if (TextUtils.isEmpty(email)) {
+            binding.emailAlert.visibility = View.VISIBLE
+            binding.emailAlert.text = getString(R.string.email_can_t_empty)
+        } else {
+            binding.emailAlert.visibility = View.GONE
+        }
+    }
+
+    private fun validateEmail() {
+        val email = binding.emailEtRegister.text.toString().trim()
+        if (TextUtils.isEmpty(email)) {
+            binding.emailAlert.visibility = View.VISIBLE
+            binding.emailAlert.text = getString(R.string.email_can_t_empty)
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            binding.emailAlert.visibility = View.VISIBLE
+            binding.emailAlert.text = getString(R.string.invalid_email)
+        } else {
+            binding.emailAlert.visibility = View.GONE
+        }
+    }
+
+    private fun validatePassword() {
+        val password = binding.passwordEtRegister.text.toString().trim()
+        if (TextUtils.isEmpty(password)) {
+            binding.passwordAlert.text = getString(R.string.password_can_t_empty)
+            binding.passwordAlert.visibility = View.VISIBLE
+        } else if (password.length < MIN_PASSWORD_LENGTH) {
+            binding.passwordAlert.text = getString(R.string.password_must_be_at_least_8_characters_long)
+            binding.passwordAlert.visibility = View.VISIBLE
+        } else {
+            binding.passwordAlert.visibility = View.GONE
+        }
+    }
+
+
+    private fun validateFields(){
+        val email = binding.emailEtRegister.text.toString().trim()
+        val password = binding.passwordEtRegister.text.toString().trim()
+        val name = binding.nameEtRegister.text.toString().trim()
+
+        val isEmailValid = !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches()
+        val isPasswordValid = !TextUtils.isEmpty(password) && password.length >= MIN_PASSWORD_LENGTH
+        val isNameValid = !TextUtils.isEmpty(name)
+
+        binding.btnRegister.isEnabled = isEmailValid && isPasswordValid && isNameValid
+    }
+
+    companion object {
+        private const val MIN_PASSWORD_LENGTH = 8
     }
 }
