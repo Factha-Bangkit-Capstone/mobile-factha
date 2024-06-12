0cb5202a72132f1dc6416fa3a0d06393c3c8e53e
izamulfikri <mfikri765@gmail.com>
2024-06-12 17:50:42 +0700

    Add Datastore for Profile

--- app/src/main/java/com/bangkit/factha/data/network/ApiServiceMain.kt
+++ app/src/main/java/com/bangkit/factha/data/network/ApiServiceMain.kt
@@ -1,4 +1,15 @@
 package com.bangkit.factha.data.network
 
+import com.bangkit.factha.data.response.ProfileResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Path
+
 interface ApiServiceMain {
+    @GET("users/{userId}")
+    suspend fun getProfile(
+        @Path("userId") userId: String,
+        @Header("X-Auth-Token") token: String
+    ): Response<ProfileResponse>
 }
