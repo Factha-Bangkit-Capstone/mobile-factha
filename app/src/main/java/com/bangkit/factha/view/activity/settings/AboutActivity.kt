ff4c1fce8990329b7d2d24ebee834a89b46ebbe7
Muhammad Dzaka Murran Rusid <99970931+dzkmrn@users.noreply.github.com>
2024-06-12 17:47:00 +0700

    Adding About Application Layout and Logic

--- app/src/main/java/com/bangkit/factha/view/activity/settings/AboutActivity.kt
+++ app/src/main/java/com/bangkit/factha/view/activity/settings/AboutActivity.kt
@@ -1,21 +1,50 @@
 package com.bangkit.factha.view.activity.settings
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.bangkit.factha.R
+import com.bangkit.factha.databinding.ActivityAboutBinding
 
 class AboutActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAboutBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_about)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityAboutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        binding.ivLinkedin1.setOnClickListener { goToUrl("https://www.linkedin.com/in/mhmddzaka/") }
+        binding.ivLinkedin2.setOnClickListener { goToUrl("https://www.linkedin.com/in/mohammad-izamul-fikri-fahmi/") }
+        binding.ivLinkedin3.setOnClickListener { goToUrl("https://www.linkedin.com/in/fachrizala/") }
+        binding.ivLinkedin4.setOnClickListener { goToUrl("https://www.linkedin.com/in/aisya-oxzanna-pitaloka/") }
+        binding.ivLinkedin5.setOnClickListener { goToUrl("https://www.linkedin.com/in/sabila-nadia-islamia-027095220/") }
+        binding.ivLinkedin6.setOnClickListener { goToUrl("https://www.linkedin.com/in/maretaaliana/") }
+        binding.ivLinkedin7.setOnClickListener { goToUrl("") }
+
+        binding.ivGithub1.setOnClickListener { goToUrl("https://github.com/dzkmrn/") }
+        binding.ivGithub2.setOnClickListener { goToUrl("https://github.com/zenosance/") }
+        binding.ivGithub3.setOnClickListener { goToUrl("https://github.com/toingg/") }
+        binding.ivGithub4.setOnClickListener { goToUrl("https://github.com/aisyaoxzanna/") }
+        binding.ivGithub5.setOnClickListener { goToUrl("https://github.com/SabilaNadia02/") }
+        binding.ivGithub6.setOnClickListener { goToUrl("https://github.com/Maretaaliana") }
+        binding.ivGithub7.setOnClickListener { goToUrl("") }
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressedDispatcher.onBackPressed()
+        return true
+    }
+
+    private fun goToUrl(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(intent)
     }
 }
